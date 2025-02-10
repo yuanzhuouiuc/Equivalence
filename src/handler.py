69d5aa9cd5b6b58ea484621e8f53b6d7823791c7
yuanzhuouiuc <yzzhanghust@gmail.com>
2025-02-10 22:10:52 +0800

    change comment

--- src/handler.py
+++ src/handler.py
@@ -1,9 +1,8 @@
-
 import os
 import subprocess
 
 def read_all(fd):
-    """循环读取文件描述符 fd 中的所有数据"""
+    """read all data in fd"""
     blocks = []
     while True:
         chunk = os.read(fd, 4096)
@@ -14,7 +13,6 @@ def read_all(fd):
 
 class Handler:
     def __init__(self, exec_path):
-        # exec_path 期望为 bytes（例如 b"/path/to/executable"）
         self.exec_path = exec_path
         self.result = b""
         self.error = b""
@@ -22,11 +20,9 @@ class Handler:
         self.init()
 
     def init(self):
-        # 如有额外初始化需求，可在此添加
         pass
 
     def deinit(self):
-        # 如有额外清理需求，可在此添加
         pass
 
     def cleanup(self):
@@ -36,7 +32,7 @@ class Handler:
 
     def execute_program_subprocess(self, buffer):
         self.cleanup()
-        args = buffer.split()  # buffer 为 bytes，split() 返回 bytes 列表
+        args = buffer.split()
         cmd = [self.exec_path] + args
         env = dict(**os.environ)
         env["ASAN_OPTIONS"] = "log_to_stderr=1:abort_on_error=1:flush_on_exit=1"
@@ -46,7 +42,7 @@ class Handler:
                 cmd,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
-                timeout=10,  # 超时 10 秒
+                timeout=10,
                 env=env,
                 check=False,
                 bufsize=0
